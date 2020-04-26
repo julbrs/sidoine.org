4e418759b63d484552d3a5af4dbdbb6c392e6b85
Julien Bras <julienbras@sidoine.org>
2020-04-26 09:32:01 -0400

    make this thing case sensitive

---
+++ src/components/Layout.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Footer }  from "./footer"
+
+import "typeface-merriweather"
+import "../styles/main.css"
+import {
+  GiMountaintop,
+} from "react-icons/gi"
+
+const Layout = ({ location, title, children }) => {
+  return (
+    <>
+    <header className="bg-white py-6">
+      <div className="container text-center">
+        <Link to="/" className="inline-block">
+          <GiMountaintop size="5em"/>
+        </Link>
+      </div>
+    </header>
+    {children}
+    <div className="bg-white py-8 lg:py-16 mt-8">
+      <div className="container">
+      </div> 
+    </div>
+    <Footer />
+  </>
+  )
+}
+
+export default Layout
