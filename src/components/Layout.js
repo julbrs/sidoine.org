7cd5fa465d7dba861154f542d59e077050c189de
Julien Bras <julienbras@sidoine.org>
2020-04-26 10:25:28 -0400

    footer to Footer in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Footer }  from "./footer"
+import { Footer }  from "./Footer"
 
 import "typeface-merriweather"
 import "../styles/main.css"
